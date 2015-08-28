66a07b688e920763c0987eeee1d5436ede4f3edf
J T <jtasharski@gmail.com>
2015-08-28 12:15:22 -0700

    global chat

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var app = angular.module('crisisApp', ['ngResource', 'firebase']);
 app.controller('MainCtrl', ['$scope', '$http', '$firebaseArray', function ($scope, $http, $firebaseArray) {
 	var ref = new Firebase("https://crisis.firebaseio.com");
-	$scope.messages = $firebaseArray(ref);
+	var globalRef = ref.child("globalChat");
+	$scope.messages = $firebaseArray(globalRef);
   $scope.sendMessage = function(e){
   	if ((e.keyCode === 13) && $scope.msg) {
-	  	$scope.messages.$add({from: "Obama", body: $scope.msg});
+	  	$scope.messages.$add({from: "Duck", body: $scope.msg});
 	  	$scope.msg = "";
 	  }
  	}
